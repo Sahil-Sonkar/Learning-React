53e1e75c4e987af3f1da71f64288fb61bb5923f7
Sahil Sonkar <sonkarsahil04@gmail.com>
2025-03-31 20:45:32 +0530

    added a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
 import './App.css'
 
 function App() {
-  return (<h1>Vite + React</h1>)
+  return (<><h1>Vite + React</h1><ul>
+    <li>example</li>
+    <li>exop</li>
+  </ul></>
+  )
 }
 
 export default App
